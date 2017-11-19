62ef408babbcd4a88c09d4126f77aacb6d575ca4
Rafael Bachmann <mediumendian@gmail.com>
2017-11-19 22:42:23 +0100

    debug printing

--- raPID/pid_1d.cpp
+++ raPID/pid_1d.cpp
@@ -482,71 +482,6 @@ void calculatePID_angular_rate() {
     pid_last_error = pid_error;
 }
 
-
-// ————————————————————————————————————————————————————
-// ———             SERIAL DEBUG OUTPUT              ———
-// ————————————————————————————————————————————————————
-
-void print_yaw_pitch_roll() {
-    Serial.print(F("yaw_pitch_roll\t"));
-    Serial.print(yaw_pitch_roll[YAW_ANGLE]);
-    Serial.print(F("\t"));
-    Serial.print(yaw_pitch_roll[PITCH_ANGLE]);
-    Serial.print(F("\t"));
-    Serial.println(yaw_pitch_roll[ROLL_ANGLE]);
-}
-
-void print_attitude() {
-    Serial.print(F("Attitude\t"));
-    Serial.print(attitude[YAW_ANGLE]);
-    Serial.print(F("\t"));
-    Serial.print(attitude[PITCH_ANGLE]);
-    Serial.print(F("\t"));
-    Serial.println(attitude[ROLL_ANGLE]);
-}
-
-void print_receiver() {
-    Serial.print(receiver_in[THROTTLE_CHANNEL]);
-    Serial.print(F("\t"));
-    Serial.print(receiver_in[ROLL_CHANNEL]);
-    Serial.print(F("\t"));
-    Serial.print(receiver_in[PITCH_CHANNEL]);
-    Serial.print(F("\t"));
-    Serial.println(receiver_in[YAW_CHANNEL]);
-}
-
-void print_angular_rates() {
-    Serial.print(gyro_axis[ROLL_RATE]);
-    Serial.print("\t");
-    Serial.print(gyro_axis[PITCH_RATE]);
-    Serial.print("\t");
-    Serial.println(gyro_axis[YAW_RATE]);
-}
-
-void print_binary(int value, int num_places) {
-    int mask = 0;
-
-    for (int n = 1; n <= num_places; n++) {
-        mask = (mask << 1) | 0x0001;
-    }
-    value = value & mask; // truncate v to specified number of places
-
-    while (num_places) {
-
-        if (value & (0x0001 << (num_places - 1))) {
-            serial_print(F("1"));
-        } else {
-            serial_print(F("0"));
-        }
-
-        --num_places;
-        if (((num_places % 4) == 0) && (num_places != 0)) {
-            serial_print("_");
-        }
-    }
-    serial_println();
-}
-
 void inline watchdog_init() {
     WDOG_UNLOCK = WDOG_UNLOCK_SEQ1;
     WDOG_UNLOCK = WDOG_UNLOCK_SEQ2;
@@ -696,4 +631,85 @@ void read_yaw() {
             (uint16_t)(micros() - receiver_in_start[YAW_CHANNEL]);
         input_flags |= 1 << YAW_CHANNEL;
     }
+}
+
+
+/*
+   ————————————————————————————————————————————————————
+   ———             SERIAL DEBUG OUTPUT              ———
+   ————————————————————————————————————————————————————
+*/
+
+void print_yaw_pitch_roll() {
+    Serial.print(F("yaw_pitch_roll\t"));
+    Serial.print(yaw_pitch_roll[YAW_ANGLE]);
+    Serial.print(F("\t"));
+    Serial.print(yaw_pitch_roll[PITCH_ANGLE]);
+    Serial.print(F("\t"));
+    Serial.println(yaw_pitch_roll[ROLL_ANGLE]);
+}
+
+void print_attitude() {
+    Serial.print(F("Attitude\t"));
+    Serial.print(attitude[YAW_ANGLE]);
+    Serial.print(F("\t"));
+    Serial.print(attitude[PITCH_ANGLE]);
+    Serial.print(F("\t"));
+    Serial.println(attitude[ROLL_ANGLE]);
+}
+
+void print_receiver() {
+    Serial.print(receiver_in[THROTTLE_CHANNEL]);
+    Serial.print(F("\t"));
+    Serial.print(receiver_in[ROLL_CHANNEL]);
+    Serial.print(F("\t"));
+    Serial.print(receiver_in[PITCH_CHANNEL]);
+    Serial.print(F("\t"));
+    Serial.println(receiver_in[YAW_CHANNEL]);
+}
+
+void print_angular_rates() {
+    Serial.print(gyro_axis[ROLL_RATE]);
+    Serial.print("\t");
+    Serial.print(gyro_axis[PITCH_RATE]);
+    Serial.print("\t");
+    Serial.println(gyro_axis[YAW_RATE]);
+}
+
+void print_binary(int value, int num_places) {
+    int mask = 0;
+
+    for (int n = 1; n <= num_places; n++) {
+        mask = (mask << 1) | 0x0001;
+    }
+    /* truncate v to specified number of places */
+    value = value & mask;
+
+    while (num_places) {
+
+        if (value & (0x0001 << (num_places - 1))) {
+            serial_print(F("1"));
+        } else {
+            serial_print(F("0"));
+        }
+
+        --num_places;
+        if (((num_places % 4) == 0) && (num_places != 0)) {
+            serial_print("_");
+        }
+    }
+    serial_println();
+}
+
+int print_all(int num, ...) {
+    char* line;
+    va_list argList;
+    va_start(argList, num);
+
+    for(; num; num--) {
+        line = va_arg(argList, char*);
+        Serial.print("line: ");
+        Serial.println(line);
+    }
+    va_end(argList);
 }
