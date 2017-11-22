f26e9677892e365e8c94bc13e3a874d24d7378e2
Rafael Bachmann <mediumendian@gmail.com>
2017-11-22 14:54:03 +0100

    remove unused headers

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@
 
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-#include "MPU6050_9Axis_MotionApps41.h"
+//#include "MPU6050_9Axis_MotionApps41.h"
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
