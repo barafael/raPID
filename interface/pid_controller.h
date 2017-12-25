40b643606f5995ae2acf8c7a08ba1df1e41aa269
Rafael Bachmann <mediumendian@gmail.com>
2017-12-25 22:34:37 +0100

    add better modular pid controller implementation
    based on the notes from Brett Beauregard in his piece 'improving the beginner's PID'

---
+++ interface/pid_controller.h
@@ -0,0 +1,45 @@
+#ifndef PID_CONTROLLER_H
+#define PID_CONTROLLER_H
+
+#include <stdint.h>
+
+class pid_result {
+    public:
+        float sum;
+};
+
+class pid_controller {
+    public:
+        float kp;
+        float ki;
+        float kd;
+
+        pid_controller(const float kp, const float ki, const float kd,
+                const float integral_limit, const float output_limit);
+ 
+        pid_result compute(const uint64_t now, const float measured,
+                const float setpoint);
+
+        pid_controller* set_p(const float kp);
+        pid_controller* set_i(const float ki);
+        pid_controller* set_d(const float kd);
+
+    private:
+        float integral;
+        float integral_limit;
+
+        float derivative;
+
+        /* For derivative-on-error */
+        float last_error;
+
+        /* For derivative-on-setpoint */
+        float last_setpoint;
+
+        float output_limit;
+
+        uint64_t last_time;
+};
+
+#endif // PID_CONTROLLER_H
+
---
+++ src/pid_controller.cpp
@@ -0,0 +1,71 @@
+#include "../interface/pid_controller.h"
+
+pid_controller::pid_controller(const float kp, const float ki, const float kd,
+        const float integral_limit, const float output_limit) {
+
+    this->kp = kp;
+    this->ki = ki;
+    this->kd = kd;
+
+    this->integral_limit = integral_limit;
+    this->output_limit = output_limit;
+};
+
+
+/* TODO: handle overflows for 'now' using rollover or somesuch. */
+pid_result pid_controller::compute(const uint64_t now, const float measured,
+        const float setpoint) {
+
+    uint32_t elapsed_time = now - last_time;
+
+    pid_result result { 0.0 };
+
+    float error = measured - setpoint;
+
+    /* Proportional term */
+    result.sum += this->kp * error;
+
+    /* Integral term */
+    this->integral += elapsed_time * error * this->ki;
+    /* Integral windup limit */
+    this->integral = this->integral > this->integral_limit ?
+        this->integral_limit : this->integral;
+    this->integral = this->integral < this->integral_limit * -1 ?
+        this->integral_limit * -1 : this->integral;
+
+    result.sum += this->integral;
+
+    /* Derivative term on error */
+    result.sum += ((error - last_error) / elapsed_time) * this->kd;
+
+    /* Derivative term on input */
+    // result.sum += (setpoint - last_setpoint) / elapsed_time;
+
+    /* Output limit */
+    result.sum = result.sum > this->output_limit ?
+        this->output_limit : result.sum;
+    result.sum = result.sum < this->output_limit * -1 ?
+        this->output_limit * -1 : result.sum;
+
+    this->last_error = error;
+    this->last_time = now;
+    this->last_setpoint = setpoint;
+
+    return result;
+}
+
+pid_controller* pid_controller::set_p(const float kp) {
+    this->kp = kp;
+    return this;
+}
+
+pid_controller* pid_controller::set_i(const float ki) {
+    this->ki = ki;
+    return this;
+}
+
+pid_controller* pid_controller::set_d(const float kd) {
+    this->kd = kd;
+    return this;
+}
+
