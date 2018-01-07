5b4f12328a7a4edfdcfbe3c82bedb01d3887824c
Rafael Bachmann <mediumendian@gmail.com>
2018-01-07 17:16:19 +0100

    adjust pins for mini board

--- interface/pins.h
+++ interface/pins.h
@@ -3,24 +3,24 @@
 
 static const uint8_t DEBUG_PIN = 15;
 
-static const uint8_t LED_PIN = 13;
+static const uint8_t LED_PIN = 14;
 
-static const uint8_t MPU_INTERRUPT_PIN = 14;
+static const uint8_t MPU_INTERRUPT_PIN = 13;
 
 typedef enum {
-    THROTTLE_INPUT_PIN  = 11,
-    ROLL_INPUT_PIN      = 10,
-    PITCH_INPUT_PIN     = 9,
-    YAW_INPUT_PIN       = 8,
-    AUX1_INPUT_PIN      = 7,
-    AUX2_INPUT_PIN      = 6
+    THROTTLE_INPUT_PIN  = 12,
+    ROLL_INPUT_PIN      = 11,
+    PITCH_INPUT_PIN     = 10,
+    YAW_INPUT_PIN       = 9,
+    AUX1_INPUT_PIN      = 8,
+    AUX2_INPUT_PIN      = 7
 } input_pin;
 
 typedef enum {
-    LEFT_SERVO_PIN  = 20,
-    RIGHT_SERVO_PIN = 21,
-    FRONT_SERVO_PIN = 22,
-    BACK_SERVO_PIN  = 23
+    LEFT_SERVO_PIN  = 6,
+    RIGHT_SERVO_PIN = 5,
+    FRONT_SERVO_PIN = 4,
+    BACK_SERVO_PIN  = 3
 } output_pin;
 
 #endif // PINS_H
