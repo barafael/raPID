35281fb19c2d5d545696daf920ff67857d111ddc
Rafael Bachmann <mediumendian@gmail.com>
2018-01-07 01:29:40 +0100

    remove old enum

--- interface/settings.h
+++ interface/settings.h
@@ -12,12 +12,6 @@ const static uint16_t ARM_TIMEOUT = 2500;
 const static uint16_t CONFIG_THRESHOLD = 1150;
 const static uint16_t CONFIG_TIMEOUT = 1500;
 
-typedef enum {
-    ROLL_RATE  = 0,
-    PITCH_RATE = 1,
-    YAW_RATE   = 2
-} angle_rate;
-
 static const uint8_t NUM_CHANNELS = 6;
 
 typedef enum {
