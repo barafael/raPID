c94073d33cdb16b1fc45a29695a258ed71bba07a
Rafael Bachmann <mediumendian@gmail.com>
2018-01-09 00:54:40 +0100

    Add initial vector implementation

---
+++ interface/Vector3.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_3_H
+#define VECTOR_3_H
+
+template <class T> class Vector3 {
+    T values[3];
+
+    public:
+    Vector3(T x, T y, T z) : values{ x, y, z } {
+    }
+    T &operator[](size_t index) {
+        return values[index % 3];
+    }
+};
+
+#endif // VECTOR_3_H
