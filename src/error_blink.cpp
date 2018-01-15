caad9890d7e308b18c7917cbdd23e7f9602ff923
Rafael Bachmann <mediumendian@gmail.com>
2018-01-15 16:51:00 +0100

    nicer error blink

--- src/error_blink.cpp
+++ src/error_blink.cpp
@@ -6,7 +6,7 @@
 static const uint16_t BLINK_PERIOD = 500;
 
 void blink_pattern(const char *pattern) {
-    while (1) {
+    while (true) {
         for (size_t index = 0; index < strlen(pattern); index++) {
             switch (pattern[index]) {
                 case '0':
@@ -28,9 +28,9 @@ void blink_pattern(const char *pattern) {
 void error_blink(error_type error, const char *message) {
     Serial.println(message);
     switch (error) {
-        case DMP_INIT_MEM_LOAD_FAILED: blink_pattern("1100"); break;
-        case DMP_CONF_UPDATES_FAILED: blink_pattern("1010"); break;
-        case DMP_ERROR_UNKNOWN: blink_pattern("0001"); break;
-        default: blink_pattern("10100000"); break;
+        case DMP_INIT_MEM_LOAD_FAILED: blink_pattern("1100");     break;
+        case DMP_CONF_UPDATES_FAILED:  blink_pattern("1010");     break;
+        case DMP_ERROR_UNKNOWN:        blink_pattern("0001");     break;
+        default:                       blink_pattern("10100000"); break;
     }
 }
