4231047b8875a6bdb8374b58630fe526871fe0f8
Rafael Bachmann <mediumendian@gmail.com>
2018-01-22 18:44:33 +0100

    mixer constructor

--- include/Mixer.h
+++ include/Mixer.h
@@ -8,6 +8,12 @@ class Mixer {
         float roll_volume;
         float pitch_volume;
         float yaw_volume;
+
+        Mixer(float thr_vol, float roll_vol, float pitch_vol, float yaw_vol)
+            : throttle_volume { thr_vol }
+            , roll_volume     { roll_vol }
+            , pitch_volume    { pitch_vol }
+            , yaw_volume      { yaw_vol } {}
 };
 
 #endif //MIXER_H
