8a3ed5f0a9d900550111e43c6a5e9f620a3e40b8
Rafael Bachmann <mediumendian@gmail.com>
2018-01-28 20:55:20 +0100

    set lower timeout for state change

--- include/settings.h
+++ include/settings.h
@@ -5,9 +5,9 @@
 
 static const uint16_t DISARM_TIMEOUT = 2500;
 
-static const uint16_t ARM_TIMEOUT = 2500;
+static const uint16_t ARM_TIMEOUT = 1000;
 
-static const uint16_t CONFIG_TIMEOUT = 1500;
+static const uint16_t CONFIG_TIMEOUT = 2500;
 
 static const size_t NUM_CHANNELS = 6;
 
@@ -16,6 +16,7 @@ typedef enum {
     ROLL_CHANNEL     = 1,
     PITCH_CHANNEL    = 2,
     YAW_CHANNEL      = 3,
+
     AUX1_CHANNEL     = 4,
     AUX2_CHANNEL     = 5
 } input_channel;
