00317a33e3c44949e361d1676ceb7e440a9349c3
Rafael Bachmann <mediumendian@gmail.com>
2018-01-29 22:21:05 +0100

    add fastpwm output class

---
+++ include/FastPWMOutput.h
@@ -0,0 +1,60 @@
+#ifndef FAST_PWM_OUTPUT
+#define FAST_PWM_OUTPUT
+
+#include "Arduino.h"
+
+#include "Output.h"
+
+#include "./util.h"
+ 
+class FastPWMOutput : Output {
+    private:
+        uint16_t milli_throttle = 0;
+
+        uint8_t resolution_bits = 16;
+        float frequency_hz = 400.0f;
+        float wavelength_sec = 1.0f/frequency_hz;
+
+        float min_pulse_width_sec = 0.001f;
+        float max_pulse_width_sec = 0.002f;
+
+        uint8_t min_dutycycle_percent = min_pulse_width_sec/wavelength_sec * 100;
+        uint8_t max_dutycycle_percent = max_pulse_width_sec/wavelength_sec * 100;
+
+        uint16_t lower_limit = (1 << resolution_bits) / 100 * min_dutycycle_percent;
+        uint16_t upper_limit = (1 << resolution_bits) / 100 * max_dutycycle_percent;
+
+        uint16_t throttle_low_cutoff = 25;
+        uint16_t base_pulse_ms = 800;
+
+        uint16_t range = upper_limit - lower_limit;
+
+        void write(uint16_t _milli_throttle);
+
+    public:
+        FastPWMOutput(const uint8_t pin,
+                float throttle_volume,
+                float roll_volume, float pitch_volume, float yaw_volume)
+            : Output(pin, throttle_volume, roll_volume, pitch_volume, yaw_volume) {
+                pinMode(pin, OUTPUT);
+            }
+
+        void apply(uint16_t throttle,
+                float roll_stbl, float pitch_stbl, float yaw_stbl);
+
+        void set_resolution(uint8_t res);
+        void set_frequency(uint32_t freq);
+
+        void shut_off();
+
+        void set_limits(uint16_t lower, uint16_t upper);
+
+        void set_throttle_volume(float volume);
+
+        void set_roll_volume (float volume);
+        void set_pitch_volume(float volume);
+        void set_yaw_volume  (float volume);
+};
+
+#endif // FAST_PWM_OUTPUT
+
---
+++ src/FastPWMOutput.cpp
@@ -0,0 +1,83 @@
+#include "../include/FastPWMOutput.h"
+
+/* TODO implement arming for ESC type? */
+/* TODO implement flight mode offset? */
+/* TODO implement servo/ESC subclasses? */
+/* TODO better model for position/thrust/endpoints */
+/* TODO remove includes when not using warning output */
+ 
+void FastPWMOutput::shut_off() {
+    write(0);
+}
+
+/* Avoiding invert functionality, maybe simplify? */
+void FastPWMOutput::write(uint16_t _milli_throttle) {
+    this->milli_throttle = _milli_throttle > 1000 ? 1000 : _milli_throttle;
+    uint16_t thrust = (uint32_t) (this->range * milli_throttle) / 1000;
+    float value = thrust / 1000.0f;
+    analogWrite(pin, lower_limit + range * value);
+}
+
+void FastPWMOutput::apply(uint16_t _milli_throttle,
+        float roll_stbl, float pitch_stbl, float yaw_stbl) {
+    /* Throttle cutoff to avoid spinning props due to movement when throttle is low but state is armed
+     * Do it here, before the control values are added up
+     */
+    if (_milli_throttle < throttle_low_cutoff) {
+        shut_off();
+        return;
+    }
+
+    /* intermediary int16_t to prevent overflow */
+    int16_t throttle_tmp = _milli_throttle;
+
+    throttle_tmp =  (int16_t) (_milli_throttle * mixer.throttle_volume);
+
+    throttle_tmp += (int16_t) (roll_stbl       * mixer.roll_volume);
+    throttle_tmp += (int16_t) (pitch_stbl      * mixer.pitch_volume);
+    throttle_tmp += (int16_t) (yaw_stbl        * mixer.yaw_volume);
+
+    clamp(throttle_tmp, 0, 1000);
+
+    write(throttle_tmp);
+}
+
+void FastPWMOutput::set_resolution(uint8_t res) {
+    if (res <= 16) {
+        resolution_bits = res;
+    }
+}
+
+void FastPWMOutput::set_frequency(uint32_t freq) {
+        frequency_hz = freq;
+}
+
+void FastPWMOutput::set_limits(uint16_t lower, uint16_t upper) {
+    if (upper < lower) {
+        Serial.print("Dubious limits given to output on pin ");
+        Serial.println(pin);
+        uint16_t tmp = lower;
+        lower = upper;
+        upper = tmp;
+    }
+    upper_limit = upper > 1300 ? 1300 : upper;
+    lower_limit = lower;
+
+    range = upper_limit - lower_limit;
+}
+
+void FastPWMOutput::set_throttle_volume(float volume) {
+    mixer.throttle_volume = volume;
+}
+
+void FastPWMOutput::set_roll_volume(float volume) {
+    mixer.roll_volume = volume;
+}
+
+void FastPWMOutput::set_pitch_volume(float volume) {
+    mixer.pitch_volume = volume;
+}
+
+void FastPWMOutput::set_yaw_volume(float volume) {
+    mixer.yaw_volume = volume;
+}
