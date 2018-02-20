13a8269a4fffae03dee0f46d8ff2775820981270
Rafael Bachmann <mediumendian@gmail.com>
2018-02-20 15:37:49 +0100

    add raw imu interface

---
+++ include/imu/RawIMU.h
@@ -0,0 +1,21 @@
+#ifndef RAW_IMU_H
+#define RAW_IMU_H
+
+#include <stdint.h>
+#include <array>
+
+#include "axis.h"
+
+/* This class represents an interface to an absolutely unspectacular IMU sensor
+ * that consists just of a gyroscope and accelerometer combo without and
+ * internal motion processing. */
+
+class RawIMU {
+    private:
+    public:
+        virtual void update_angular_rates(axis_t& angular_rates) = 0;
+        virtual void update_acceleration(axis_t& acceleration) = 0;
+};
+
+#endif // RAW_IMU_H
+
