c45487d2110e80dbe375d0424863a498e2d21be6
Rafael Bachmann <mediumendian@gmail.com>
2018-02-23 20:22:33 +0100

    fix pins

--- include/pins.h
+++ include/pins.h
@@ -13,19 +13,19 @@ static const uint8_t SENTRAL_POWER = 14;
 static const uint8_t SENTRAL_INTERRUPT_PIN = 8;
 
 typedef enum {
-    THROTTLE_INPUT_PIN  = 23,
-    ROLL_INPUT_PIN      = 22,
-    PITCH_INPUT_PIN     = 21,
-    YAW_INPUT_PIN       = 20,
-    AUX1_INPUT_PIN      = 19,
-    AUX2_INPUT_PIN      = 18
+    THROTTLE_INPUT_PIN = 22,
+    ROLL_INPUT_PIN     = 20,
+    PITCH_INPUT_PIN    = 21,
+    YAW_INPUT_PIN      = 23,
+    AUX1_INPUT_PIN     = 19,
+    AUX2_INPUT_PIN     = 18,
 } input_pin;
 
 typedef enum {
     LEFT_SERVO_PIN  = 5,
     RIGHT_SERVO_PIN = 4,
     FRONT_SERVO_PIN = 3,
-    BACK_SERVO_PIN  = 2
+    BACK_SERVO_PIN  = 6
 } output_pin;
 
 #endif // PINS_H
