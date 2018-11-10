9efa8c72dae72ec6f39054ec769928de2dcf5e0a
Rafael Bachmann <mediumendian@gmail.com>
2018-11-10 22:35:20 +0100

    port ppm receiver

--- include/receiver/PPMReceiver.hpp
+++ include/receiver/PPMReceiver.hpp
@@ -1,29 +1,36 @@
 #ifndef PPMRECEIVER_H
 #define PPMRECEIVER_H
 
+#include<stdbool.h>
+
 #include "PulsePosition.h"
+#include "../settings.h"
+#include "../util.h"
 
-#include "settings.h"
-#include "Receiver.hpp"
+typedef struct {
+    uint8_t input_pin;
+    PulsePositionInput ppm_rx;
 
-class PPMReceiver : Receiver {
-    private:
-        uint8_t input_pin;
-        PulsePositionInput ppm_rx;
+    int16_t ppm_translate[NUM_CHANNELS] = { 2, 0, 1, 3, 4, 5 };
 
-        channels_t ppm_translate = { 2, 0, 1, 3, 4, 5 };
+    /* Channel offsets and throttle zero-point */
+    int16_t offsets[NUM_CHANNELS];
 
-    public:
-        PPMReceiver(uint8_t _input_pin, channels_t offsets);
+    /* Per-channel trims */
+    int16_t trims[NUM_CHANNELS];
 
-        const void update(channels_t channels) override;
+    /* Per-channel inversion */
+    bool inversion[NUM_CHANNELS];
+} PPMReceiver_t;
 
-        void set_offsets(channels_t offsets) override;
-        void set_trims(channels_t trims) override;
-        void set_inversion(inversion_t inversion) override;
+PPMReceiver_t init_ppm_receiver(uint8_t _input_pin, int16_t *offsets);
 
-        const bool has_signal() override;
-};
+const void receiver_update(int16_t *channels);
+
+void set_offsets(int16_t *offsets);
+void set_trims(int16_t *trims);
+void set_inversion(bool *inversion);
+
+const bool has_signal();
 
 #endif // PPMRECEIVER_H
-
--- src/receiver/PPMReceiver.cpp
+++ src/receiver/PPMReceiver.cpp
@@ -1,7 +1,8 @@
 #include "../../include/receiver/PPMReceiver.hpp"
 
-PPMReceiver::PPMReceiver(uint8_t _input_pin, channels_t offsets) {
-    switch (_input_pin) {
+PPMReceiver_t init_ppm_receiver(uint8_t input_pin, int16_t *offsets) {
+    PPMReceiver_t receiver;
+    switch (input_pin) {
         /* Filter pins that are ppm-capable */
         case 5:
         case 6:
@@ -14,48 +15,49 @@ PPMReceiver::PPMReceiver(uint8_t _input_pin, channels_t offsets) {
             break;
         default:
             Serial.print("Pin ");
-            Serial.print(_input_pin);
+            Serial.print(input_pin);
             Serial.println(" cannot be used as pulse position input.");
     }
 
-    input_pin = _input_pin;
+    receiver.input_pin = input_pin;
 
     /* TODO use init list? */
     for (size_t index = 0; index < NUM_CHANNELS; index++) {
-        this->offsets[index] = offsets[index];
+        receiver.offsets[index] = offsets[index];
     }
-    ppm_rx.begin(input_pin);
+    receiver.ppm_rx.begin(input_pin);
+    return receiver;
 }
 
-const void PPMReceiver::update(channels_t channels) {
-    int num = ppm_rx.available();
+const void receiver_update(PPMReceiver_t *self, int16_t *channels) {
+    int num = self->ppm_rx.available();
     if (num > 0) {
         for (size_t index = 0; index < NUM_CHANNELS; index++) {
-            float val = ppm_rx.read(index + 1);
+            float val = self->ppm_rx.read(index + 1);
             clamp(val, 1000, 2000);
-            if (inversion[index]) {
+            if (self->inversion[index]) {
                 val = 2000 - (val - 1000);
             }
-            val += offsets[index];
-            val += trims[index];
-            channels[ppm_translate[index]] = (int16_t) val;
+            val += self->offsets[index];
+            val += self->trims[index];
+            channels[self->ppm_translate[index]] = (int16_t) val;
         }
     }
 }
 
-void PPMReceiver::set_trims(channels_t trims) {
+void set_trims(PPMReceiver_t *self, int16_t *trims) {
     for (size_t index = 0; index < NUM_CHANNELS; index++) {
-        this->trims[index] = trims[index];
+        self->trims[index] = trims[index];
     }
 }
 
-void PPMReceiver::set_inversion(inversion_t inversion) {
+void set_inversion(PPMReceiver_t *self, bool *inversion) {
     for (size_t index = 0; index < NUM_CHANNELS; index++) {
-        this->inversion[index] = inversion[index];
+        self->inversion[index] = inversion[index];
     }
 }
 
 /* TODO less pointless function */
-const bool PPMReceiver::has_signal() {
+const bool has_signal() {
     return true;
 }
