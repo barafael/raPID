a3f94e318b3b0e022e3e49592c7b4e7d447e2fa5
Rafael Bachmann <mediumendian@gmail.com>
2019-03-04 18:23:35 +0100

    better watchdog implementation

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -11,26 +11,31 @@
     assigns WDOG_TOVALL;
     assigns WDOG_TOVALH;
     assigns WDOG_PRESC;
+    assigns ghost_interrupt_status;
 
     // actually, these are volatile registers and not memory
     //ensures WDOG_UNLOCK == WDOG_UNLOCK_SEQ2;
     //ensures WDOG_PRESC == 4;
+
+    ensures ghost_interrupt_status == INTERRUPTS_ON;
 */
 void init_watchdog() {
     mock_noInterrupts();
 
-    WDOG_UNLOCK = WDOG_UNLOCK_SEQ1;                         // unlock access to WDOG registers
+    // unlock access to WDOG registers
+    WDOG_UNLOCK = WDOG_UNLOCK_SEQ1;
     WDOG_UNLOCK = WDOG_UNLOCK_SEQ2;
-    mock_delayMicroseconds(1);                                   // Need to wait a bit..
+    mock_delayMicroseconds(1);
 
-    // for this demo, we will use 1 second WDT timeout (e.g. you must reset it in < 1 sec or a boot occurs)
+    // 1 second WDT timeout (e.g. reset in < 1 sec or a reset occurs)
     WDOG_TOVALH = 0x006d;
     WDOG_TOVALL = 0xdd00;
 
-    // This sets prescale clock so that the watchdog timer ticks at 7.2MHz
+    // watchdog timer at 7.2MHz
     WDOG_PRESC  = 0x400;
 
-    // Set options to enable WDT. You must always do this as a SINGLE write to WDOG_CTRLH
+    // Enable WDT.
+    // This must happen in one single write to WDOG_CTRLH
     WDOG_STCTRLH |= WDOG_STCTRLH_ALLOWUPDATE |
         WDOG_STCTRLH_WDOGEN | WDOG_STCTRLH_WAITEN |
         WDOG_STCTRLH_STOPEN | WDOG_STCTRLH_CLKSRC;
